e9c798e8cee860a80bd323f68b8a571ca004ebdd
James Manwell <manwell@umass.edu>
2025-02-23 10:49:51 -0500

    Initial commit

---
+++ Hybrid_temp.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 10:41:21 2025
+
+@author: jfman
+"""
+__version__ = "1.0.11"
+import numpy as np
+pi = np.pi
+print("pi",pi)
+print("!!!",pi)
+print("@@@@",pi)
+print("&&&&",pi)
+print("####&&&&",pi)
+print("####&&&&",pi)
+print("####&&&&",pi)
+print("####&&&&",pi)
+print("####&&&&",pi)
