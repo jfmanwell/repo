663d7140d23e7426168ce8db4bd240fda4627f7e
James Manwell <manwell@umass.edu>
2025-02-24 15:02:46 -0500

    test 2

--- Hybrid_temp.py
+++ Hybrid_temp.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 22 10:41:21 2025
 
-@author: jfman
-"""
-__version__ =  "1.0.11"
+__version__ = "1.0.11"
 import numpy as np
 pi = np.pi
 print("pi",pi)
