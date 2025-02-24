138cc2d64603afb2c116afe0bd7b703213d171d0
James Manwell <manwell@umass.edu>
2025-02-24 15:05:20 -0500

    test 2

--- Hybrid_temp.py
+++ Hybrid_temp.py
@@ -1,11 +1,12 @@
 
-__version__ = "1.0.13"
+__version__ = "1.0.12"
 import numpy as np
 pi = np.pi
 print("pi",pi)
 print("!!!",pi)
 print("!!!",pi)
 print("@@@@",pi)
+
 print("imaginary")
 print("aardvark")
 print("@@@@",pi)
